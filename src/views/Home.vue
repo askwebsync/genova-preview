39d9b9b2cc6fb4d35bb2913ee557a17fdb3d6a4b
rafikarim16 <karemrafi@gmail.com>
2022-10-24 12:29:58 +0700

    sisa detailed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="flex flex-col gap-4">
-    <Banner></Banner>
     <TrendingProducts />
     <NewProducts />
     <HomepageProducts />
@@ -9,13 +8,12 @@
 </template>
 
 <script>
-import Banner from "../components/homepage/HomepageBanner.vue";
 import TrendingProducts from "../components/homepage/TrendingProducts.vue";
 import HomepageProducts from "../components/homepage/HomepageProducts.vue";
 import NewProducts from "../components/homepage/NewProducts.vue";
 
 export default {
-  components: { Banner, TrendingProducts, HomepageProducts, NewProducts },
+  components: { TrendingProducts, HomepageProducts, NewProducts },
   data() {
     return {};
   },
