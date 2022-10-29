15f7eec48972209d95633e4e6b6ebe969f952afb
rafikarim16 <karemrafi@gmail.com>
2022-10-29 19:57:34 +0700

    banner homepage

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,7 +4,7 @@
       <nav
         class="container px-4 py-7 mx-auto md:px-6 md:items-center lg:px-8 xl:px-12"
       >
-        <div class="flex items-center justify-between">
+        <div class="flex items-center justify-between gap-6">
           <!-- Mobile menu button -->
           <div @click="showMenu = !showMenu" class="flex md:hidden">
             <button type="button" class="text-black focus:outline-none">
@@ -27,6 +27,69 @@
               class="focus:outline-none"
             />
           </router-link>
+          <ul
+            :class="showMenu ? 'flex' : 'hidden'"
+            class="flex-col md:justify-around mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-4 md:order-1"
+          >
+            <li v-if="showMenu" class="relative mt-3 md:hidden">
+              <div
+                class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"
+              >
+                <svg
+                  class="w-5 h-5"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <input
+                type="text"
+                id="search-navbar"
+                v-model="searchQuery"
+                class="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search..."
+                @focusout="showDiv = !showDiv"
+                @focusin="showDiv = !showDiv"
+              />
+            </li>
+            <li
+              @click="showMenu = !showMenu"
+              class="text-base font-semibold text-black hover:text-yellow-600 focus:outline-none"
+            >
+              <router-link to="/products">Product</router-link>
+            </li>
+            <li
+              @click="showMenu = !showMenu"
+              class="text-base font-semibold text-black hover:text-yellow-600 focus:outline-none"
+            >
+              <router-link to="/recipe">Recipe</router-link>
+            </li>
+            <li
+              @click="showMenu = !showMenu"
+              class="text-base font-semibold text-black hover:text-yellow-600 focus:outline-none"
+            >
+              <router-link to="/about">About Us</router-link>
+            </li>
+            <li
+              @click="showMenu = !showMenu"
+              class="text-base font-semibold text-black hover:text-yellow-600 focus:outline-none"
+            >
+              <router-link to="/contact">Contact</router-link>
+            </li>
+            <li
+              @click="showMenu = !showMenu"
+              class="text-base font-semibold text-black hover:text-yellow-600 focus:outline-none"
+            >
+              <router-link to="/location">Location</router-link>
+            </li>
+          </ul>
 
           <div class="flex md:order-2 md:w-7/12 md:mx-auto">
             <div
@@ -54,7 +117,7 @@
               <input
                 type="text"
                 id="search-navbar"
-                class="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="block p-2 ml-12 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search..."
                 v-model="searchQuery"
                 @focusout="showDiv = !showDiv"
@@ -121,69 +184,6 @@
           </div>
         </div> -->
         <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-        <ul
-          :class="showMenu ? 'flex' : 'hidden'"
-          class="flex-col md:justify-around mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-4 md:order-1"
-        >
-          <li v-if="showMenu" class="relative mt-3 md:hidden">
-            <div
-              class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"
-            >
-              <svg
-                class="w-5 h-5"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </div>
-            <input
-              type="text"
-              id="search-navbar"
-              v-model="searchQuery"
-              class="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search..."
-              @focusout="showDiv = !showDiv"
-              @focusin="showDiv = !showDiv"
-            />
-          </li>
-          <li
-            @click="showMenu = !showMenu"
-            class="text-base font-semibold text-black hover:text-yellow-600 focus:outline-none"
-          >
-            <router-link to="/products">Product</router-link>
-          </li>
-          <li
-            @click="showMenu = !showMenu"
-            class="text-base font-semibold text-black hover:text-yellow-600 focus:outline-none"
-          >
-            <router-link to="/recipe">Recipe</router-link>
-          </li>
-          <li
-            @click="showMenu = !showMenu"
-            class="text-base font-semibold text-black hover:text-yellow-600 focus:outline-none"
-          >
-            <router-link to="/about">About Us</router-link>
-          </li>
-          <li
-            @click="showMenu = !showMenu"
-            class="text-base font-semibold text-black hover:text-yellow-600 focus:outline-none"
-          >
-            <router-link to="/contact">Contact</router-link>
-          </li>
-          <li
-            @click="showMenu = !showMenu"
-            class="text-base font-semibold text-black hover:text-yellow-600 focus:outline-none"
-          >
-            <router-link to="/location">Location</router-link>
-          </li>
-        </ul>
       </nav>
     </div>
   </div>
