345974f4fbbb82c5d90e6bbb41ff06f8fb80396a
rafikarim16 <karemrafi@gmail.com>
2022-11-02 18:04:18 +0700

    clear excep product detail page

--- src/components/PageLoader.vue
+++ src/components/PageLoader.vue
@@ -1,233 +1,157 @@
 <template>
-  <!-- <div class="middle" v-if="!isloaded">
-    <div class="bar bar1"></div>
-    <div class="bar bar2"></div>
-    <div class="bar bar3"></div>
-    <div class="bar bar4"></div>
-    <div class="bar bar5"></div>
-    <div class="bar bar6"></div>
-    <div class="bar bar7"></div>
-    <div class="bar bar8"></div>
-  </div> -->
-  <div class="breeding-rhombus-spinner" v-if="!isloaded">
-    <div class="rhombus child-1"></div>
-    <div class="rhombus child-2"></div>
-    <div class="rhombus child-3"></div>
-    <div class="rhombus child-4"></div>
-    <div class="rhombus child-5"></div>
-    <div class="rhombus child-6"></div>
-    <div class="rhombus child-7"></div>
-    <div class="rhombus child-8"></div>
-    <div class="rhombus big"></div>
+  <div class="container">
+    <div class="large-indicator">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   data: () => {
-    return {
-      isloaded: false,
-    };
-  },
-  mounted() {
-    document.onreadystatechange = () => {
-      if (document.readyState == "complete") {
-        this.isloaded = true;
-      }
-    };
+    return {};
   },
 };
 </script>
 
 <style scoped>
-.breeding-rhombus-spinner {
-  height: 65px;
-  width: 65px;
-  position: relative;
-  transform: rotate(45deg);
+.container {
+  --action-primary-color: rgba(0, 159, 227, 1);
+  --alert-watched-color: rgba(255, 255, 0, 1);
+  --warning-new-color: rgba(255, 0, 175, 1);
 }
 
-.breeding-rhombus-spinner,
-.breeding-rhombus-spinner * {
+.container {
+  align-items: center;
+  -webkit-align-items: center;
+  background-color: rgba(0, 0, 0, 1);
   box-sizing: border-box;
+  display: flex;
+  display: -webkit-flex;
+  font-size: 16px;
+  justify-content: center;
+  -webkit-justify-content: center;
+  height: 100vh;
+  width: 100%;
 }
 
-.breeding-rhombus-spinner .rhombus {
-  height: calc(65px / 7.5);
-  width: calc(65px / 7.5);
-  animation-duration: 2000ms;
-  top: calc(65px / 2.3077);
-  left: calc(65px / 2.3077);
-  background-color: #bdb76b;
-  position: fixed;
-  top: 40%;
-  left: 50%;
-  z-index: 999;
+.large-indicator {
+  display: flex;
+  display: -webkit-flex;
+}
+
+.large-indicator div {
+  height: 1em;
+  margin-right: 1em;
+  width: 1em;
+}
+
+.large-indicator div {
+  animation-duration: 0.9s, 2.7s;
+  -webkit-animation-duration: 0.9s, 2.7s;
+  animation-name: blink, rainbow;
+  -webkit-animation-name: blink, rainbow;
   animation-iteration-count: infinite;
+  -webkit-animation-iteration-count: infinite;
 }
 
-.breeding-rhombus-spinner .rhombus:nth-child(2n + 0) {
-  margin-right: 0;
-}
-
-.breeding-rhombus-spinner .rhombus.child-1 {
-  animation-name: breeding-rhombus-spinner-animation-child-1;
-  animation-delay: calc(100ms * 1);
-}
-
-.breeding-rhombus-spinner .rhombus.child-2 {
-  animation-name: breeding-rhombus-spinner-animation-child-2;
-  animation-delay: calc(100ms * 2);
-}
-
-.breeding-rhombus-spinner .rhombus.child-3 {
-  animation-name: breeding-rhombus-spinner-animation-child-3;
-  animation-delay: calc(100ms * 3);
-}
-
-.breeding-rhombus-spinner .rhombus.child-4 {
-  animation-name: breeding-rhombus-spinner-animation-child-4;
-  animation-delay: calc(100ms * 4);
-}
-
-.breeding-rhombus-spinner .rhombus.child-5 {
-  animation-name: breeding-rhombus-spinner-animation-child-5;
-  animation-delay: calc(100ms * 5);
-}
-
-.breeding-rhombus-spinner .rhombus.child-6 {
-  animation-name: breeding-rhombus-spinner-animation-child-6;
-  animation-delay: calc(100ms * 6);
-}
-
-.breeding-rhombus-spinner .rhombus.child-7 {
-  animation-name: breeding-rhombus-spinner-animation-child-7;
-  animation-delay: calc(100ms * 7);
-}
-
-.breeding-rhombus-spinner .rhombus.child-8 {
-  animation-name: breeding-rhombus-spinner-animation-child-8;
-  animation-delay: calc(100ms * 8);
-}
-
-.breeding-rhombus-spinner .rhombus.big {
-  height: calc(65px / 3);
-  width: calc(65px / 3);
-  animation-duration: 2000ms;
-  top: calc(65px / 3);
-  left: calc(65px / 3);
-  background-color: #bdb76b;
-  animation: breeding-rhombus-spinner-animation-child-big 2s infinite;
-  animation-delay: 0.5s;
-}
-
-@keyframes breeding-rhombus-spinner-animation-child-1 {
-  50% {
-    transform: translate(-325%, -325%);
-  }
-}
-
-@keyframes breeding-rhombus-spinner-animation-child-2 {
-  50% {
-    transform: translate(0, -325%);
-  }
-}
-
-@keyframes breeding-rhombus-spinner-animation-child-3 {
-  50% {
-    transform: translate(325%, -325%);
-  }
-}
-
-@keyframes breeding-rhombus-spinner-animation-child-4 {
-  50% {
-    transform: translate(325%, 0);
-  }
-}
-
-@keyframes breeding-rhombus-spinner-animation-child-5 {
-  50% {
-    transform: translate(325%, 325%);
-  }
-}
-
-@keyframes breeding-rhombus-spinner-animation-child-6 {
-  50% {
-    transform: translate(0, 325%);
-  }
-}
-
-@keyframes breeding-rhombus-spinner-animation-child-7 {
-  50% {
-    transform: translate(-325%, 325%);
-  }
-}
-
-@keyframes breeding-rhombus-spinner-animation-child-8 {
-  50% {
-    transform: translate(-325%, 0);
-  }
-}
-
-@keyframes breeding-rhombus-spinner-animation-child-big {
-  50% {
-    transform: scale(0.5);
-  }
-}
-.middle {
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  position: absolute;
-}
-.bar {
-  width: 10px;
-  height: 70px;
-  background: #fff;
-  display: inline-block;
-  transform-origin: bottom center;
-  border-top-right-radius: 20px;
-  border-top-left-radius: 20px;
-  /*   box-shadow:5px 10px 20px inset rgba(255,23,25.2); */
-  animation: loader 1.2s linear infinite;
-}
-.bar1 {
-  animation-delay: 0.1s;
-}
-.bar2 {
-  animation-delay: 0.2s;
-}
-.bar3 {
+.large-indicator div:nth-of-type(2) {
   animation-delay: 0.3s;
-}
-.bar4 {
-  animation-delay: 0.4s;
-}
-.bar5 {
-  animation-delay: 0.5s;
-}
-.bar6 {
-  animation-delay: 0.6s;
-}
-.bar7 {
-  animation-delay: 0.7s;
-}
-.bar8 {
-  animation-delay: 0.8s;
+  -webkit-animation-delay: 0.3s;
 }
 
-@keyframes loader {
+.large-indicator div:nth-of-type(3) {
+  animation-delay: 0.6s;
+  -webkit-animation-delay: 0.6s;
+}
+
+@keyframes blink {
   0% {
-    transform: scaleY(0.1);
-    background: transparent;
+    opacity: 0;
   }
-  50% {
-    transform: scaleY(1);
-    background: yellowgreen;
+
+  33% {
+    opacity: 1;
   }
+
+  66% {
+    opacity: 1;
+  }
+
   100% {
-    transform: scaleY(0.1);
-    background: transparent;
+    opacity: 0;
+  }
+}
+
+@-webkit-keyframes blink {
+  0% {
+    opacity: 0;
+  }
+
+  33% {
+    opacity: 1;
+  }
+
+  66% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+  }
+}
+
+@keyframes rainbow {
+  0% {
+    background-color: var(--action-primary-color);
+  }
+
+  33% {
+    background-color: var(--action-primary-color);
+  }
+
+  33.5% {
+    background-color: var(--warning-new-color);
+  }
+
+  66.5% {
+    background-color: var(--warning-new-color);
+  }
+
+  67% {
+    background-color: var(--alert-watched-color);
+  }
+
+  100% {
+    background-color: var(--alert-watched-color);
+  }
+}
+
+@-webkit-keyframes rainbow {
+  0% {
+    background-color: var(--action-primary-color);
+  }
+
+  33% {
+    background-color: var(--action-primary-color);
+  }
+
+  33.5% {
+    background-color: var(--warning-new-color);
+  }
+
+  66.5% {
+    background-color: var(--warning-new-color);
+  }
+
+  67% {
+    background-color: var(--alert-watched-color);
+  }
+
+  100% {
+    background-color: var(--alert-watched-color);
   }
 }
 </style>
