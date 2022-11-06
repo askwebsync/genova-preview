4f31855d784b9a87061a492d369ede83b5793d18
rafikarim16 <karemrafi@gmail.com>
2022-11-06 16:54:45 +0700

    rapihin semua

--- src/components/contactpage/Contact.vue
+++ src/components/contactpage/Contact.vue
@@ -17,7 +17,7 @@
           class="flex flex-col items-center gap-4 background-yellow p-8 card"
         >
           <i class="card-icon far fa-envelope"></i>
-          <h3 class="text-lg text-white mt-6">
+          <h3 class="text-lg text-white m-6 max-w-xs block">
             genovaindonesiaofficial@gmail.com
           </h3>
         </div>
@@ -25,7 +25,7 @@
           class="flex flex-col items-center gap-4 background-yellow p-8 card"
         >
           <i class="card-icon fas fa-phone"></i>
-          <h3 class="text-lg text-white mt-6">
+          <h3 class="text-lg text-white m-6">
             <a href="tel:081212128171" class="hover:text-gray-400"
               >+62 812-1212-8171</a
             >
@@ -36,7 +36,7 @@
         >
           <i class="card-icon fas fa-map-marker-alt"></i>
 
-          <h3 class="text-lg text-white my-6">
+          <h3 class="text-lg text-white m-6">
             Komplek PU Jl. B No. 18 RC Veteran - Tanah Kusir, Jakarta Selatan
             12330
           </h3>
@@ -60,6 +60,6 @@
 }
 
 .card:hover .card-icon {
-  transform: scale(1.6);
+  transform: scale(1.4);
 }
 </style>
