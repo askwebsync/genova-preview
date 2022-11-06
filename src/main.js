6eddad022f371830c8635d21f27b84ea3380f4dc
rafikarim16 <karemrafi@gmail.com>
2022-11-06 23:45:59 +0700

    rapihin + button backtotop

--- src/main.js
+++ src/main.js
@@ -5,6 +5,5 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/css/main.css";
 import "tw-elements";
-import BackToTop from "vue-backtotop";
 
-createApp(App).use(createPinia()).use(router).use(BackToTop).mount("#app");
+createApp(App).use(createPinia()).use(router).mount("#app");
