27136ef20f5ad4bcc38c46ef1c691e23e5f46a1a
rafikarim16 <karemrafi@gmail.com>
2022-11-08 15:20:52 +0700

    finalisasi

--- src/components/homepage/HomepageBanner.vue
+++ src/components/homepage/HomepageBanner.vue
@@ -1,94 +1,74 @@
 <template>
   <section class="container mx-auto">
     <div
-      id="carouselDarkVariant"
-      class="carousel slide carousel-fade carousel-dark relative"
+      id="carouselExampleIndicators"
+      class="carousel slide relative"
       data-bs-ride="carousel"
     >
-      <!-- Indicators -->
       <div
         class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4 space-x-4"
       >
         <button
-          data-bs-target="#carouselDarkVariant"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
           data-bs-slide-to="0"
           class="active"
           aria-current="true"
           aria-label="Slide 1"
         ></button>
         <button
-          data-bs-target="#carouselDarkVariant"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
           data-bs-slide-to="1"
-          class="ml-2"
           aria-label="Slide 2"
         ></button>
         <button
-          data-bs-target="#carouselDarkVariant"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
           data-bs-slide-to="2"
-          class="ml-2"
           aria-label="Slide 3"
         ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="3"
+          aria-label="Slide 4"
+        ></button>
       </div>
-
-      <!-- Inner -->
       <div class="carousel-inner relative w-full overflow-hidden">
-        <!-- Single item -->
-        <div class="carousel-item active relative float-left w-full">
+        <div class="carousel-item active float-left w-full">
           <img
             src="../../assets/images/banner/1.jpg"
             class="block w-full"
-            alt="Motorbike Smoke"
+            alt="Wild Landscape"
           />
-          <div class="carousel-caption hidden md:block absolute text-center">
-            <!-- <h5 class="text-xl">First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p> -->
-          </div>
         </div>
-
-        <!-- Single item -->
-        <div class="carousel-item relative float-left w-full">
+        <div class="carousel-item float-left w-full">
           <img
             src="../../assets/images/banner/2.jpg"
             class="block w-full"
-            alt="Mountaintop"
+            alt="Camera"
           />
-          <div class="carousel-caption hidden md:block absolute text-center">
-            <!-- <h5 class="text-xl">Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p> -->
-          </div>
         </div>
-
-        <!-- Single item -->
-        <div class="carousel-item relative float-left w-full">
+        <div class="carousel-item float-left w-full">
           <img
             src="../../assets/images/banner/3.jpg"
             class="block w-full"
-            alt="Woman Reading a Book"
+            alt="Exotic Fruits"
           />
-          <div class="carousel-caption hidden md:block absolute text-center">
-            <!-- <h5 class="text-xl">Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p> -->
-          </div>
         </div>
-        <div class="carousel-item relative float-left w-full">
+        <div class="carousel-item float-left w-full">
           <img
             src="../../assets/images/banner/4.jpg"
             class="block w-full"
-            alt="Woman Reading a Book"
+            alt="Exotic Fruits"
           />
-          <div class="carousel-caption hidden md:block absolute text-center">
-            <!-- <h5 class="text-xl">Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p> -->
-          </div>
         </div>
       </div>
-      <!-- Inner -->
-
-      <!-- Controls -->
-      <!-- <button
+      <button
         class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
         type="button"
-        data-bs-target="#carouselDarkVariant"
+        data-bs-target="#carouselExampleIndicators"
         data-bs-slide="prev"
       >
         <span
@@ -100,7 +80,7 @@
       <button
         class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
         type="button"
-        data-bs-target="#carouselDarkVariant"
+        data-bs-target="#carouselExampleIndicators"
         data-bs-slide="next"
       >
         <span
@@ -108,7 +88,7 @@
           aria-hidden="true"
         ></span>
         <span class="visually-hidden">Next</span>
-      </button> -->
+      </button>
     </div>
   </section>
 </template>
