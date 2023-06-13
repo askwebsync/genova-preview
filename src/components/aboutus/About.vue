ce9b7ed2599aa3b9bbf78fb21867480f4e319ce0
ask.websync <ask.websync@gmail.com>
2023-06-13 20:11:05 +0700

    contact & location

--- src/components/aboutus/About.vue
+++ src/components/aboutus/About.vue
@@ -1,45 +1,49 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <section class="container mx-auto">
-    <div class="p-4 lg:p-6 lg:pt-10 pb-12 xl:p-12">
-      <div class="text-center center mb-12">
-        <h1 class="pcolor mb-1 text-xl lg:text-xxl">About Us</h1>
-        <hr class="h-1 rounded background-navbar w-32 bg-black border-0" />
-      </div>
-      <div class="about-section">
-        <div
-          class="inner-container bg-gray-200 inner-container-shadow justify-center p-12"
-        >
-          <ul class="text-left space-y-12">
-            <li>
-              <p class="leading-6 tracking-normal font-medium">
-                GENOVA saat ini adalah perusahaan yang bergerak memproduksi
-                Premium Syrup, Powder Drink dan Fruitblend dengan harga yang
-                terjangkau. Menggunakan bahan baku impor berkualitas,
-                bersertifikasi halal serta diproduksi di Jakarta,
-                Indonesia.Sehingga ketersediaan stock untuk pasokan bahan baku
-                minuman selalu terjaga.
-              </p>
-            </li>
-            <li>
-              <p class="leading-6 tracking-normal font-medium">
-                Produk Genova juga sudah memiliki sertifikat BPOM di beberapa
-                produk unggulan dan terus mengupdate semua produknya agar
-                memiliki sertifikat BPOM. Sehingga produk kami aman untuk
-                digunakan sebagai bahan baku minuman
-              </p>
-            </li>
-            <li>
-              <p class="leading-6 tracking-normal font-medium">
-                Berdiri sejak tahun 2016, GENOVA saat ini sudah dipercaya oleh
-                lebih dari 1000 pelanggan setia dari hotel bintang lima maupun
-                cafe papan atas di seluruh Indonesia. Product GENOVA banyak
-                digunakan untuk campuran pembuatan mocktail, cocktail,
-                milkshake, ice blend, hot drink dan lain-lain. Tersedia dalam
-                ragam varian rasa.
-              </p>
-            </li>
-          </ul>
+  <section>
+    <div class="container mx-auto px-4 py-6 md:px-8 lg:px-24 lg:py-10">
+      <div class="flex flex-col gap-8 lg:gap-10">
+        <div class="flex flex-col items-center text-center">
+          <h1 class="mb-1 text-xl lg:text-2xl uppercase">About</h1>
+          <hr
+            class="h-1 rounded w-full lg:w-40 bg-black border-0 mx-auto lg:mx-0"
+          />
+        </div>
+        <div class="about-section">
+          <div
+            class="inner-container bg-gray-200 inner-container-shadow justify-center p-12"
+          >
+            <ul class="text-left space-y-12">
+              <li>
+                <p class="leading-6 tracking-normal font-medium">
+                  GENOVA saat ini adalah perusahaan yang bergerak memproduksi
+                  Premium Syrup, Powder Drink dan Fruitblend dengan harga yang
+                  terjangkau. Menggunakan bahan baku impor berkualitas,
+                  bersertifikasi halal serta diproduksi di Jakarta,
+                  Indonesia.Sehingga ketersediaan stock untuk pasokan bahan baku
+                  minuman selalu terjaga.
+                </p>
+              </li>
+              <li>
+                <p class="leading-6 tracking-normal font-medium">
+                  Produk Genova juga sudah memiliki sertifikat BPOM di beberapa
+                  produk unggulan dan terus mengupdate semua produknya agar
+                  memiliki sertifikat BPOM. Sehingga produk kami aman untuk
+                  digunakan sebagai bahan baku minuman
+                </p>
+              </li>
+              <li>
+                <p class="leading-6 tracking-normal font-medium">
+                  Berdiri sejak tahun 2016, GENOVA saat ini sudah dipercaya oleh
+                  lebih dari 1000 pelanggan setia dari hotel bintang lima maupun
+                  cafe papan atas di seluruh Indonesia. Product GENOVA banyak
+                  digunakan untuk campuran pembuatan mocktail, cocktail,
+                  milkshake, ice blend, hot drink dan lain-lain. Tersedia dalam
+                  ragam varian rasa.
+                </p>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -48,8 +52,8 @@
 
 <style scoped>
 .about-section {
-  background: url(../../../public/assets/images/banner/About/About-Banner.jpeg)
-    no-repeat left;
+  background: url(/assets/images/banner/Homepage/homepage-banner.png) no-repeat
+    left;
   background-size: fill;
   background-position: center center;
   background-color: #fdfdfd;
--- src/components/contactpage/Contact.vue
+++ src/components/contactpage/Contact.vue
@@ -1,84 +1,39 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <section class="container mx-auto">
-    <nav class="flex ml-3 mt-5 mb-10 md:ml-10" aria-label="Breadcrumb">
-      <ol class="inline-flex items-center space-x-1 md:space-x-3">
-        <li class="inline-flex items-center">
-          <a
-            href="/"
-            class="inline-flex items-center text-sm font-medium hover:text-yellow-600"
-          >
-            <svg
-              class="w-4 h-4 mr-2"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
-              ></path>
-            </svg>
-            Home
-          </a>
-        </li>
-        <li aria-current="page">
-          <div class="flex items-center">
-            <svg
-              class="w-6 h-6 text-gray-400"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            <span
-              class="ml-1 text-sm font-medium md:ml-2 pcolor hover:text-yellow-600"
-              >Contact</span
-            >
-          </div>
-        </li>
-      </ol>
-    </nav>
-    <div class="flex flex-col my-12">
-      <div class="mb-12 text-center center">
-        <h1 class="pcolor text-2xl">Contact Us</h1>
-        <hr class="h-1 rounded background-navbar w-32 bg-black border-0" />
+  <section class="container mx-auto px-4 py-6 md:px-8 lg:px-24 lg:py-10">
+    <div class="flex flex-col gap-8 lg:gap-10">
+      <div class="flex flex-col items-center text-center">
+        <h1 class="mb-1 text-xl lg:text-2xl uppercase">Contact Us</h1>
+        <hr
+          class="h-1 rounded w-full lg:w-40 bg-black border-0 mx-auto lg:mx-0"
+        />
         <div class="my-3">
           <p class="text-lg">Get in touch with us for more information</p>
         </div>
       </div>
 
-      <div
-        class="mb-12 grid md:grid-cols-2 lg:grid-cols-3 self-center gap-12 px-12 md:flex-row lg:px-12"
-      >
+      <div class="grid md:grid-cols-2 xl:grid-cols-3 self-center gap-12">
         <div
-          class="flex flex-col items-center gap-4 background-yellow p-8 card"
+          class="flex flex-col items-center gap-4 bg-product-home p-8 rounded-lg shadow-lg max-w-sm"
         >
-          <i class="card-icon far fa-envelope"></i>
-          <h3 class="text-white font-bold center mx-2">
+          <i class="card-icon far fa-envelope text-yellow-500"></i>
+          <h3 class="text-black font-bold whitespace-normal">
             genovaindonesiaofficial@gmail.com
           </h3>
         </div>
         <div
-          class="flex flex-col items-center gap-4 background-yellow p-8 card"
+          class="flex flex-col items-center gap-4 bg-product-home p-8 rounded-lg shadow-lg max-w-sm"
         >
-          <i class="card-icon fas fa-phone"></i>
-          <h3 class="text-white font-bold center mx-2">
-            <a href="tel:081212128171" class="hover:text-black"
-              >+62 812-1212-8171</a
-            >
+          <i class="card-icon fas fa-phone text-yellow-500"></i>
+          <h3 class="text-black font-bold">
+            <a href="tel:081212128171" class=""> +62 812-1212-8171 </a>
           </h3>
         </div>
         <div
-          class="flex flex-col items-center gap-4 background-yellow p-8 card"
+          class="flex flex-col items-center gap-4 bg-product-home p-8 rounded-lg shadow-lg max-w-sm"
         >
-          <i class="card-icon fas fa-map-marker-alt"></i>
-
-          <h3 class="text-white font-bold center mx-2">
+          <i class="card-icon fas fa-map-marker-alt text-yellow-500"></i>
+          <h3 class="text-black font-bold">
             Komplek PU Jl. B No. 18 RC Veteran - Tanah Kusir, Jakarta Selatan
             12330
           </h3>
@@ -93,7 +48,7 @@
 .card-icon {
   margin-top: 12px;
   font-size: 28px;
-  background: white;
+  background-color: whitesmoke;
   width: 60px;
   height: 60px;
   text-align: center;
